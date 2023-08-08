d25ebdd4d34287a89a859df8b8477695c30b0af5
neeraj-gs <gsneeraj05@gmail.com>
2023-08-08 20:06:01 +0530

    Added Routers to move along diffrent pages

--- Course Selling App/src/App.jsx
+++ Course Selling App/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Signup from './Signup'
 import Appbar from './Appbar'
 import Signin from './Singin'
@@ -6,10 +7,14 @@ import Signin from './Singin'
 const App = () => {
   return (
     <div style={{width:"100vw",height:"100vh",backgroundColor:"#eeeeee"}}>
-      <Appbar />
-      {/* <Signup /> */}
-      <Signin />
 
+    <Router>
+    <Appbar />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
     </div>
   )
 }
--- Course Selling App/src/Appbar.jsx
+++ Course Selling App/src/Appbar.jsx
@@ -6,8 +6,16 @@ const Appbar = () => {
     <div style={{display:'flex',justifyContent:'space-between',alignContent:'center',alignItems:'center',padding:10}}>
         <Typography variant='h4'>Coursera</Typography>
         <div >
-        <Button style={{marginRight:20}} variant='contained'>Sign Up</Button>
-        <Button variant='contained'>Sign in</Button>
+        <Button style={{marginRight:20}} variant='contained'
+            onClick={()=>{
+                window.location="/signup"
+            }}
+        >Sign Up</Button>
+        <Button variant='contained'
+            onClick={()=>{
+                window.location="/signin"
+            }}
+        >Sign in</Button>
         </div>
         
     </div>
