8893906a3a516e0d262448d54eaa9186a840e2ff
neeraj-gs <gsneeraj05@gmail.com>
2023-08-08 19:24:06 +0530

    Added a Sign in COMponet instead of a signup

--- Course Selling App/src/App.jsx
+++ Course Selling App/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Signup from './Signup'
 import Appbar from './Appbar'
+import Signin from './Singin'
 
 const App = () => {
   return (
     <div style={{width:"100vw",height:"100vh",backgroundColor:"#eeeeee"}}>
       <Appbar />
-      <Signup />
+      {/* <Signup /> */}
+      <Signin />
+
     </div>
   )
 }
---
+++ Course Selling App/src/Singin.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Button,TextField,Card,Typography} from '@mui/material'
+
+const Signin = () => {
+  return (
+    <div style={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',paddingTop:200}}>
+      <Typography fontSize={30} variant={'h3'} fontStyle={'italic'} fontWeight={1000}>Welcome Back to Coursify.</Typography>
+        <Typography fontSize={20} fontStyle={'italic'}>Sign In</Typography>
+      <div >
+        
+        <Card variant="outlined" style={{width:600,padding:30,height:300}}>
+
+        <div>
+          <div style={{display:'flex',justifyContent:'space-evenly',padding:10}}>
+          <Button variant="contained">Sign up with Facebook</Button>
+            <Button variant="contained">Sign up with Google</Button>
+          </div>
+        
+            <br />
+            <TextField fullWidth label="Username" variant="outlined" />
+            <br />
+            <br />
+            <TextField fullWidth label="Password" variant="outlined" />
+            <br />
+            <br />
+            <Button variant="contained">Sign Up</Button>
+        </div>
+
+        </Card>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Signin
