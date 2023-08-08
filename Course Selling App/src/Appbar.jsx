a553ee6aff2169ef092159fa572056940cb1ef2e
neeraj-gs <gsneeraj05@gmail.com>
2023-08-08 21:44:16 +0530

    Used useNavigate hook to stop reloading of page

--- Course Selling App/src/Appbar.jsx
+++ Course Selling App/src/Appbar.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import {Typography,Button} from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const Appbar = () => {
+    const navigate = useNavigate();
+
   return (
     <div style={{display:'flex',justifyContent:'space-between',alignContent:'center',alignItems:'center',padding:10}}>
         <Typography variant='h4'>Coursera</Typography>
         <div >
         <Button style={{marginRight:20}} variant='contained'
             onClick={()=>{
-                window.location="/signup"
+                // window.location="/signup"
+                navigate("/signup")
             }}
         >Sign Up</Button>
         <Button variant='contained'
             onClick={()=>{
-                window.location="/signin"
+                // window.location="/signin"
+                navigate("/signin")
             }}
         >Sign in</Button>
         </div>
