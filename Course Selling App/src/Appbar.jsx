522d54ff7e2dde835eab61a88948270b931ab715
neeraj-gs <gsneeraj05@gmail.com>
2023-08-08 19:03:24 +0530

    Added a AppBar compoent to render appbar at the top of the website

---
+++ Course Selling App/src/Appbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Typography,Button} from '@mui/material'
+
+const Appbar = () => {
+  return (
+    <div>
+        <Typography>Coursera</Typography>
+        <Button>Sign Up</Button>
+        <Button>Sign in</Button>
+    </div>
+  )
+}
+
+export default Appbar
