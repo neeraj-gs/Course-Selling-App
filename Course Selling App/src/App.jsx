868b7fd9001d5b25831c40092f443b3b79d8a674
neeraj-gs <gsneeraj05@gmail.com>
2023-08-08 19:03:53 +0530

    Added a Appbar component to render the Appbar on website

--- Course Selling App/src/App.jsx
+++ Course Selling App/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Signup from './Signup'
+import Appbar from './Appbar'
 
 const App = () => {
   return (
     <div style={{width:"100vw",height:"100vh",backgroundColor:"#eeeeee"}}>
+      <Appbar />
       <Signup />
     </div>
   )
