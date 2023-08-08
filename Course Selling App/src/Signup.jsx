ad2e8ec5a0e81193019228166a5f506db3f01288
neeraj-gs <gsneeraj05@gmail.com>
2023-08-08 21:08:18 +0530

    Added the right way to take input from frontend and pass to backend

--- Course Selling App/src/Signup.jsx
+++ Course Selling App/src/Signup.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Button,TextField,Card,Typography} from '@mui/material'
 
 const Signup = () => {
+
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
   return (
     <div style={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',paddingTop:200}}>
       <Typography fontSize={30} variant={'h3'} fontStyle={'italic'} fontWeight={1000}>Welcome to Coursify.</Typography>
@@ -17,21 +21,25 @@ const Signup = () => {
           </div>
         
             <br />
-            <TextField fullWidth label="Username" variant="outlined" id="username"/>
+            <TextField fullWidth label="Username" variant="outlined" onChange={(e)=>{
+              setEmail(e.target.value);
+            }}/>
             <br />
             <br />
-            <TextField fullWidth label="Password" variant="outlined" id="password"/>
+            <TextField fullWidth label="Password" variant="outlined" onChange={(e)=>{
+              setPassword(e.target.value)
+            }}/>
             <br />
             <br />
             <Button variant="contained"
               onClick={()=>{
-                let username = document.getElementById("username").value;
-                let password = document.getElementById("password").value;
+                // let username = document.getElementById("username").value;
+                // let password = document.getElementById("password").value;
                 fetch("http://localhost:3000/admin/signup",{
                   method:"POST",
                   body:JSON.stringify({
-                    username,
-                    password
+                    username:email,
+                    password:password
                   }),
                   headers:{
                     "Content-type":"application/json"
