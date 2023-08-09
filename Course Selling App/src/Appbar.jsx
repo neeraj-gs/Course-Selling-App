e85e564ab781350148d824d57eaa3630a0014a4e
neeraj-gs <gsneeraj05@gmail.com>
2023-08-09 10:00:47 +0530

    Added a logut and conditionaly render if user was authenticated

--- Course Selling App/src/Appbar.jsx
+++ Course Selling App/src/Appbar.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Typography,Button} from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
 const Appbar = () => {
     const navigate = useNavigate();
+    const [useremail,setUserEmail]=useState(null);
 
         useEffect(()=>{
             fetch("http://localhost:3000/admin/me",{
@@ -13,11 +14,42 @@ const Appbar = () => {
                 }
             }).then((res)=>{
                 res.json().then((data)=>{
+                    if(data.username){
+                        setUserEmail(data.username)
+                    }
                     console.log(data)
                 })
             })
 
         },[])
+
+
+        if(useremail){
+
+            return (
+                <div style={{display:'flex',justifyContent:'space-between',alignContent:'center',alignItems:'center',padding:10}}>
+                    <Typography variant='h4'>Coursera</Typography>
+                    <div style={{display:'flex',justifyContent:'space-between',alignContent:'center',alignItems:'center',padding:10}}>
+                    <Button style={{marginRight:20}} variant='contained'
+                        onClick={()=>{
+                            // window.location="/signup"
+                            navigate("/admin/courses")
+                        }}
+                    >AddCourse</Button>
+                    <Typography variant='h6' style={{marginRight:15}}>{useremail}</Typography>
+                    <Button variant='contained'
+                        onClick={()=>{
+                            // window.location="/signin"
+                            localStorage.setItem("token",null)
+                            // navigate("/signup")
+                        }}
+                    >Logout</Button>
+                    </div>
+                    
+                </div>
+              )
+
+        }
 
   return (
     <div style={{display:'flex',justifyContent:'space-between',alignContent:'center',alignItems:'center',padding:10}}>
