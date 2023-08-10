6aabfd5a0bf606fc8329ae0dde47e34b218dcc32
neeraj-gs <gsneeraj05@gmail.com>
2023-08-10 09:00:26 +0530

    Added a new fetch request and a userEffect taht gets the list of courses added by admin

--- Course Selling App/src/GetCourses.jsx
+++ Course Selling App/src/GetCourses.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const GetCourses = () => {
+    const [courses,setCourses]=useState([]);
+    useEffect(()=>{
+        fetch("http://localhost:3000/admin/courses",{
+            method:"GET",
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("token")
+            }
+        }).then((res)=>{
+            res.json().then((data)=>{
+                console.log(data)
+            })
+        })
+
+    },[])
+
   return (
     <div>
         
